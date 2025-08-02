16ea5f70209169963941e5c85fa4f728bb70ea7b
RohitPModani <87776300+RohitPModani@users.noreply.github.com>
2025-08-02 18:07:51 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -41,7 +41,7 @@ origins = [
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  # Adjust for production
+    allow_origins=origins,  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
